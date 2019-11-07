7dbccb90490c23c098c851cdad4b579fe324c4a0
Anderson290 <anderson.nunes@fcamara.com.br>
2019-11-07 20:05:59 -0300

    up

---
+++ api/src/config.js
@@ -0,0 +1,5 @@
+global.SALT_KEY = 'f68892-k98179-4ca3-90f2-087wqej08';
+
+module.exports = {
+    connectionString: 'mongodb+srv://legalChatbotDb:1QazxsW229@legalchatbot-errfy.mongodb.net/test?retryWrites=true'
+}
